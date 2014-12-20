fd6f0a24ef26c91dc43568bdbc49cfc5889b2bbd
onexhovia <onexhovia@gmail.com>
2014-12-20 18:58:16 +0500

    Init repo

---
+++ README.md
@@ -0,0 +1,9 @@
+1C-Bitrix - Doctrine 2 module
+===
+
+## Install
+Simply run assuming you have installed composer.phar or composer binary:
+
+``` bash
+$ php composer.phar require citfact/doctrine dev-master
+```
---
+++ lang/ru/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the Studio Fact package.
+ *
+ * (c) Kulichkin Denis (onEXHovia) <onexhovia@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$MESS['DOCTRINE_MODULE_NAME'] = '1C-Bitrix - Doctrine 2';
+$MESS['DOCTRINE_MODULE_DESCRIPTION'] = '1C-Bitrix - Doctrine 2 module bridge';
+$MESS['PARTNER_NAME'] = 'ООО «Центр интернет технологий «ФАКТ»';
+$MESS['PARTNER_URI'] = 'http://www.studiofact.ru/';
